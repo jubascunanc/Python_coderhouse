67a3499be7ed2211a8fbac5114e00ca1a37ecfc0
Juan Jose Bascuñan <jubascunanc@udd.cl>
2023-09-20 22:45:14 -0400

    objetos

--- objetos.py
+++ objetos.py
@@ -1,14 +1,29 @@
 
 class Auto:
 
+    def _init_(self, marca, modelo,color ):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+
     def descripcion(self):
         print('este es un auto')
 
     def tocar_bocina(self):
         print('piiiiiii')
 
-auto1= Auto()
-auto2= Auto()
+    def asignar_color(self, color):
+        self.color = color
+
+    def modificar_color(self, color):
+        self.color = color
+
+    def mostrar_color(self):
+        print(self.color)
+        
+        
+auto1= Auto('Ford', 'K', 'rojo')
+auto2= Auto('Mercedez','L', 'Negro')
     
 print(auto1)
 print(auto2)
@@ -18,3 +33,5 @@ auto2.tocar_bocina()
 
 auto1.descripcion()
 auto2.descripcion()
+
+auto1.mostrar_color()
