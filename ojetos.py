6f39bf02998f8ba5fabe0e7206524b1811ab6e15
Juan Jose Bascuñan <jubascunanc@udd.cl>
2023-09-20 21:45:56 -0400

    objetos

---
+++ ojetos.py
@@ -0,0 +1,20 @@
+
+class Auto:
+
+    def descripcion(self):
+        print('este es un auto')
+
+    def tocar_bocina(self):
+        print('piiiiiii')
+
+auto1= Auto()
+auto2= Auto()
+    
+print(auto1)
+print(auto2)
+
+auto1.tocar_bocina()
+auto2.tocar_bocina()
+
+auto1.descripcion()
+auto2.descripcion()
