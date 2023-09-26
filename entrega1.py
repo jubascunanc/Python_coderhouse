4258be9badb58d954f303c24fdbcce6dfc6d4780
Juan Jose Bascuñan <jubascunanc@udd.cl>
2023-09-25 22:55:45 -0400

    entrega 1

---
+++ entrega1.py
@@ -0,0 +1,72 @@
+
+DataBAse = {}
+
+registro=(DataBAse)
+print('funcionando la Data')
+
+Verdadero = True
+Falso = False
+
+input("ingrese su nombre aquí:")
+def Usuario(nombre):
+   nombre = "Juanjose"; 
+
+if Usuario: True
+print("El Usuario es Juanjose")
+
+if not Falso:
+  print("Este no es tu nombre")
+
+print(Usuario)
+
+input("ingrese su contraseña:")
+def contraseña(password):
+   password = "19011997";
+
+if contraseña: True
+print("La contraseña es correcta")
+
+if not Falso:
+   print("Esta no es tu contraseña")
+
+input("Ingrese su edad")
+def edad(años):
+   años = "26";
+if edad: True
+print("La edad es correcta")
+
+if not Falso:
+  print("la edad no es correcta")
+
+LeerDAta=(DataBAse)
+
+print("Hola como estas")
+
+guardarArchivo=(DataBAse)
+
+
+login=(DataBAse)
+
+nombre= (registro)
+print("hola", (nombre))
+
+def agregarDatos(datos):
+   print(datos)
+   datos.append('otro dato')
+   print('otro dato')
+
+class Persona:
+   #metodo Init constructor de la clase
+   def __init__(self, nombre,contraseña,edad):
+      pass
+      self.nombre = nombre
+      self.contraseña = contraseña
+      self.edad = edad
+
+def saludar(self):
+      print(f"hola mi nombre es {self.nombre} y tengo {self.edad}")
+    
+persona1 = Persona("Juanjose", 26)
+   
+persona1.saludar()
+
